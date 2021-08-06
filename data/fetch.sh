752bb98b9ef2037902f5fe694b3ffb87b9d2f7f8
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-08-06 10:41:31 +0800

    Fixing tar command on BSD

--- data/fetch.sh
+++ data/fetch.sh
@@ -3,7 +3,7 @@
 BN=$(dirname $(readlink -f $0 || python3 -c "import sys,os;print(os.path.realpath(sys.argv[1]))" $0))
 cd $BN
 
-wget https://lab.cs.tsinghua.edu.cn/physics-data/terrain/dataset.tar.gz -O - | tar -xzf - .
+wget https://lab.cs.tsinghua.edu.cn/physics-data/terrain/dataset.tar.gz -O - | tar -xzf -
 sha256sum -c checksums.sha256
 
 if [[ $? != 0 ]]; then
